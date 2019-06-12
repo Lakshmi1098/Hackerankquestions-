6b3c6bb2b2800852afed92a67048b3a30dde34b5
Lakshmi.R <46350353+Lakshmi1098@users.noreply.github.com>
2019-06-12 11:20:10 +0530

    Update infytest 9.py

--- infytest 9.py
+++ infytest 9.py
@@ -1,8 +1,14 @@
 ******************************MAKING ANAGRAMS***************************
-We consider two strings to be anagrams of each other if the first string's letters can be rearranged to form the second string. In other words, both strings must contain the same exact letters in the same exact frequency. For example, bacdc and dcbac are anagrams, but bacdc and dcbad are not.
-Alice is taking a cryptography class and finding anagrams to be very useful. She decides on an encryption scheme involving two large strings where encryption is dependent on the minimum number of character deletions required to make the two strings anagrams. Can you help her find this number?
-Given two strings,  and , that may not be of the same length, determine the minimum number of character deletions required to make  and  anagrams. Any characters can be deleted from either of the strings.
-For example,  and . The only characters that match are the 's so we have to remove  from  and  from  for a total of  deletions.
+We consider two strings to be anagrams of each other if the first string's letters can be rearranged to form 
+the second string. In other words, both strings must contain the same exact letters in the same exact frequency. 
+For example, bacdc and dcbac are anagrams, but bacdc and dcbad are not.Alice is taking a cryptography class
+and finding anagrams to be very useful. She decides on an encryption scheme involving two large strings
+where encryption is dependent on the minimum number of character deletions required to make the two strings
+anagrams. Can you help her find this number?Given two strings,  and , that may not be 
+of the same length, determine the minimum number of character deletions required to make  
+and  anagrams. Any characters can be deleted from either of the strings.
+For example,  and . The only characters that match are the 's so we have to remove  
+from  and  from  for a total of  deletions.
 
 Sample Input
 cde
@@ -39,8 +45,9 @@ print(result)
 
 
 **********************************ALTERNATING CHARACTERS****************************
-You are given a string containing characters  and  only. Your task is to change it into a string such that there are no matching adjacent characters. To do this, you are allowed to delete zero or more characters in the string.
-Your task is to find the minimum number of required deletions.
+You are given a string containing characters  and  only. Your task is to change it into a string
+such that there are no matching adjacent characters. To do this, you are allowed to delete zero or more 
+characters in the string.Your task is to find the minimum number of required deletions.
 For example, given the string , remove an  at positions  and  to make  in deletions.
 
 
